e56fa0bf71f011bd809d6f541827c83b0eb6da68
nitinrameshuf <nitinramesh@ufl.edu>
2022-07-04 18:30:49 -0400

    code

--- elephanttrade.py
+++ elephanttrade.py
@@ -4,6 +4,15 @@ import csv, os
 #To-Do
 # Implement python visualization
 
+data = []
+count = 0
+#Below implementation , splits even if the data has comma inside it
+with open(os.path.dirname(__file__) + '/Files/export.csv') as file:
+    reader = csv.reader(file)
+    for i in reader:
+        data.append(i)
+         
+for i in data:
+    print(i)
 
-file = open(os.path.dirname(__file__) + '/Files/export.csv')
-print(file)
+
