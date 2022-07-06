b55b7d9c93e0ade32c385f581276b8c90b985143
nitinrameshuf <nitinramesh@ufl.edu>
2022-07-05 20:45:48 -0400

    code

--- db_access.py
+++ db_access.py
@@ -1,1 +1,11 @@
 #Database functions
+import mysql.connector
+
+et_db = mysql.connector.connect(
+    host = "localhost",
+    user = "u",
+    password = "y"
+)
+
+mycursor = et_db.cursor()
+mycursor.execute("Create DB")
